554d89715637e41bd68c44c8bb077d04720bbcda
HannachiMouadh <mouadh40@gmail.com>
2021-08-18 01:25:25 +0100

    message

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+      <div className="mm">
+       <p>sfsdsds</p> <input type="text" className="input-group input-group-lg"></input>
+       <p>sfsdsds</p> <input type="text" className="input-group input-group-lg"></input>
+       <p>sfsdsds</p><input type="text" className="input-group input-group-lg"></input>
+       <p>sfsdsds</p><input type="text" className="input-group input-group-lg"></input>   
+      <div className="bt"><button type="button" className="btn btn-outline-secondary cent ">Button</button></div>
+      </div>
+    
+  );
+}
+
+export default App;
